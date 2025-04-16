da02df402675f03bf88ec396e5cd80722175c0c8
DaviziinSouza <davidsouzabarros87@gmail.com>
2025-04-16 02:24:51 +0000

    Desafio Completo

--- xadrez.c
+++ xadrez.c
@@ -1,80 +1,97 @@
 #include <stdio.h>
 
+// Movimentação da Torre.
+void moverTorre(int casas)
+{
+    if (casas == 0)
+        return;
+    printf("Direita\n");
+    moverTorre(casas - 1);
+}
+
+// Movimentação da Rainha.
+void moverRainha(int casas)
+{
+    if (casas == 0)
+        return;
+    printf("Esquerda\n");
+    moverRainha(casas - 1);
+}
+
+// Movimento do Bispo com recursividade e loop aninhado
+void moverBispo(int vertical, int horizontal)
+{
+    if (vertical == 0)
+        return;
+
+    for (int i = 0; i < horizontal; i++)
+    {
+        printf("Direita\n");
+    }
+
+    printf("Cima\n\n");
+    moverBispo(vertical - 1, horizontal);
+}
+
+// Movimentação do cavalo em L.
+void moverCavalo()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Cima\n");
+    }
+
+    printf("Direita\n");
+}
+
 int main()
 {
+
     int opcao;
+    const int casasTorre = 5;
+    const int casasRainha = 8;
+    const int VerticalBispo = 3;
+    const int HorizontalBispo = 2;
 
     do
     {
-        int bispo = 1, torre, rainha = 1;
-
-        printf("MOVIMENTO DAS PEÇAS DE XADREZ\n");
-        printf("[1]. Bispo.\n");
-        printf("[2]. Rainha.\n");
-        printf("[3]. Torre.\n");
-        printf("[4]. Cavalo.\n");
-        printf("[5]. Cavalo\n");
-        printf("Escolha qual peça deseja mover.\n");
+        printf("\nMOVIMENTO DAS PEÇAS DE XADREZ\n");
+        printf("[1]. Bispo\n");
+        printf("[2]. Rainha\n");
+        printf("[3]. Torre\n");
+        printf("[4]. Cavalo\n");
+        printf("[5]. Sair\n");
+        printf("Escolha qual peça deseja mover: ");
         scanf("%d", &opcao);
+        printf("\n");
 
         switch (opcao)
         {
         case 1:
-        {
-            printf("Movimento do Bispo\n");
-
-            while (bispo <= 5)
-            {
-                printf("Cima, direita.\n");
-                bispo++;
-            }
+            printf("Movimento do Bispo:\n");
+            moverbispo(VerticalBispo, HorizontalBispo);
             break;
-        }
+
         case 2:
-        {
-            printf("Movimento da Rainha.\n");
-
-            do
-            {
-                printf("Esquerda\n");
-                rainha++;
-            } while (rainha <= 8);
+            printf("Movimento da Rainha:\n");
+            moverRainha(casasRainha);
             break;
-        }
+
         case 3:
-        {
-            printf("Movimento da Torre.\n");
-            for (torre = 1; torre <= 5; torre++)
-            {
-                printf("Direita.\n");
-            }
+            printf("Movimento da Torre:\n");
+            moverTorre(casasTorre);
             break;
-        }
+
         case 4:
-        {
-            printf("Movimento do Cavalo.\n");
-
-            for (int cavalo1 = 0; cavalo1 < 2; cavalo1++)
-            {
-                printf("Baixo.\n");
-
-                int cavalo2 = 0;
-                while (cavalo2 < 1 && cavalo1 == 1)
-                {
-                    printf("Esquerda.\n");
-                    cavalo2++;
-                }
-            }
+            printf("Movimento do Cavalo:\n");
+            moverCavalo();
             break;
-        }
-        case 5:
-        {
-            printf("Saindo...\n");
-        }
-        break;
 
+        case 5:
+            printf("Saindo...\n");
+            break;
         default:
-            printf("Opção invalida por favor tente novamente\n");
+            printf("Opção inválida. Tente novamente.\n");
         }
 
     } while (opcao != 5);
